dccfcebc6247cf95bb6f8c03fd67bf50d7be1eb9
Priya Singh <singhusha25632@gmail.com>
2024-08-25 01:14:19 -0500

    more changes

---
+++ controllers/userController.js
@@ -0,0 +1,11 @@
+const userModel = require('../models/userModel')
+
+//Login callback
+const loginController = () => {};
+
+
+//register callback
+const registerController = () => {};
+
+//export
+module.exports = {loginController, registerController};
